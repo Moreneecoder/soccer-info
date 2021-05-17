1a7ac849b2e53e6e9ef77d5f6eb9ef3107b22426
Morenikeji Bello <caeserselassie@gmail.com>
2021-05-17 18:42:55 +0100

    Write RSpec tests for User model

---
+++ spec/user_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  let(:user) { described_class.new(name: 'Someone') }
+  let(:another_user) { described_class.new(name: 'Somebody') }
+
+  describe 'validations' do
+    describe 'name' do
+      it 'must be present' do
+        user = described_class.new(name: 'JohnDoe')
+        expect(user).to be_valid
+
+        user.name = nil
+        expect(user).to_not be_valid
+      end
+
+      it 'must be unique' do
+        unique_user = described_class.create!(name: 'Another One')
+        expect(user).to be_valid
+
+        unique_user = described_class.create!(name: 'Someone')
+        expect(user).to_not be_valid
+      end
+
+      it 'must be longer than two characters' do
+        expect(another_user).to be_valid
+
+        another_user.name = 'A'
+        expect(another_user).to_not be_valid
+      end
+
+      it 'must not be longer than twenty characters' do
+
+        another_user.name = 'Abcbefghij Abcbefghijkl'
+        expect(another_user).to_not be_valid
+      end
+
+    end
+  end
+
+  describe 'associations' do
+      
+  end
+  
+end
