ae3593a8351bb19d8116bce4daa1351e02d05408
Morenikeji Bello <caeserselassie@gmail.com>
2021-05-18 00:57:30 +0100

    Write RSpec test to validate user title presence

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,5 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Article, type: :model do
   let(:user) { User.create!(name: 'FooBar') }
-  let(:article) { user.build(title: 'A Title Forever', text: 'Roses Are Red, Somethings are blue', image: 'image.jpg') }
+  let(:category) { Category.create!(name: 'Europe', priority: 1) }
+  let(:article) { user.articles.build(title: 'A Title Forever', text: 'Roses Are Red, Somethings are blue', image: 'image.jpg', category_id: category.id) }
+
+  describe 'validations:' do
+    describe 'title' do
+      it 'must be present' do
+        expect(article).to be_valid
+
+        article.title = nil
+        expect(article).to_not be_valid
+      end
+    end
+
+  end
 end
