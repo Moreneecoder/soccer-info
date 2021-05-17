576d7f26e2201080a6207086e305678c363d4e84
Morenikeji Bello <caeserselassie@gmail.com>
2021-05-17 23:25:35 +0100

    Add dependent - destroy to User model associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 20, minimum: 2 }, uniqueness: true
 
-  has_many :articles, foreign_key: :author_id
-  has_many :votes
+  has_many :articles, foreign_key: :author_id, dependent: :destroy
+  has_many :votes, dependent: :destroy
 end
