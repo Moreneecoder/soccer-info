35393373de063356a188cfca7d53ffeccde68ffc
Morenikeji Bello <caeserselassie@gmail.com>
2021-05-17 23:44:37 +0100

    Add category and votes associations to Article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,10 @@
 class Article < ApplicationRecord
+  validates :author_id, presence: true
+  validates :title, presence: true
+  validates :text, presence: true
+  validates :image, presence: true
+
   belongs_to :author, class_name: 'User', foreign_key: :author_id
+  belongs_to :category
+  has_many :votes, dependent: :destroy
 end
