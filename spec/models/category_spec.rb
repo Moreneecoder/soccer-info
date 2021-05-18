d3965f093402ffb24692314e4937405933aa1268
Morenikeji Bello <caeserselassie@gmail.com>
2021-05-18 02:26:02 +0100

    Write RSpec test for category validations

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -3,5 +3,32 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    describe 'name' do
+      it 'must be present' do
+        category = described_class.new(name: 'Africa', priority: 2)
+        expect(category).to be_valid
+
+        category.name = nil
+        expect(category).to_not be_valid
+      end
+
+      it 'must be unique' do
+        category = described_class.create(name: 'Internationals', priority: 2)
+        new_category = described_class.new(name: 'Grassroots', priority: 1)
+
+        expect(new_category).to be_valid
+      end
+    end
+  end
+
+  describe 'priority' do
+    it 'must be present' do
+      category = described_class.new(name: 'Africa', priority: 2)
+      expect(category).to be_valid
+
+      category.priority = nil
+      expect(category).to_not be_valid
+    end
+  end
 end
