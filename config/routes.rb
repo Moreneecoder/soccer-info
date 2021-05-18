8659d273e9e4dc9b18e097ae710d5f01c2e6dd65
Morenikeji Bello <caeserselassie@gmail.com>
2021-05-18 16:07:09 +0100

    Setup routes for users signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :users
+  get 'users/signin', to: 'users#new'
+  resources :users, except: [:new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
