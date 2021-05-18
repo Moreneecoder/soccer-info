ea7a8f50f86f6bd7205cf62fad7438af88e50b37
Morenikeji Bello <caeserselassie@gmail.com>
2021-05-18 02:44:10 +0100

    Move user_spec tests to spec/models directory

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,56 @@
 # frozen_string_literal: true
 
+# rubocop:disable Metrics/BlockLength
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { described_class.new(name: 'Someone') }
+  let(:another_user) { described_class.new(name: 'Somebody') }
+
+  describe 'validations' do
+    describe 'name' do
+      it 'must be present' do
+        user = described_class.new(name: 'JohnDoe')
+        expect(user).to be_valid
+
+        user.name = nil
+        expect(user).to_not be_valid
+      end
+
+      it 'must be unique' do
+        described_class.create!(name: 'Another One')
+        expect(user).to be_valid
+
+        described_class.create!(name: 'Someone')
+        expect(user).to_not be_valid
+      end
+
+      it 'must be longer than two characters' do
+        expect(another_user).to be_valid
+
+        another_user.name = 'A'
+        expect(another_user).to_not be_valid
+      end
+
+      it 'must not be longer than twenty characters' do
+        another_user.name = 'Abcbefghij Abcbefghijkl'
+        expect(another_user).to_not be_valid
+      end
+    end
+  end
+
+  describe 'associations' do
+    it 'has many articles' do
+      user = User.reflect_on_association(:articles)
+      expect(user.macro).to eq(:has_many)
+    end
+
+    it 'has many votes' do
+      user = User.reflect_on_association(:votes)
+      expect(user.macro).to eq(:has_many)
+    end
+  end
 end
+
+# rubocop:enable Metrics/BlockLength
