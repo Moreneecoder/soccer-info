84a59196fe3b82d9fddb52403cec88f7b66dccd7
Morenikeji Bello <caeserselassie@gmail.com>
2021-05-19 21:12:37 +0100

    Move hompage to categories_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'articles#index'
+  root 'categories#index'
   get 'users/signup', to: 'users#new'
   resources :users, except: [:new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
