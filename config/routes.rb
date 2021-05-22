c4bff564b4d7a6ac8a19155d4283a86ac286e735
Morenikeji Bello <caeserselassie@gmail.com>
2021-05-22 15:35:39 +0100

    Create comment model and migration

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
 
   resources :users, except: [:new]
   resources :categories, only: %i[index show]
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
