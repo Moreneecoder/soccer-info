328b12724214dcb95a43e5eddd9d02423afbeb13
Morenikeji Bello <caeserselassie@gmail.com>
2021-06-01 01:17:09 +0100

    Move articles list rendering to helper logic

--- app/helpers/category_helper.rb
+++ app/helpers/category_helper.rb
@@ -19,7 +19,7 @@ module CategoryHelper
                           class: 'categories-img-height background-img-setting position-relative') do
                             
           category_display_name(category) + category_display_link(category)
-          
+
         end
       end
     end
@@ -39,4 +39,57 @@ module CategoryHelper
       link_to category.articles.last.title, category.articles.last, class: 'text-white text-decoration-none'
     end
   end
+
+  def show_category_format
+    count_image = 2
+    format_ = 'image-first'
+
+    content = ''
+
+    for article in @articles
+      if format_ == 'image-first'
+        content << category_image(article) + category_article(article)
+
+        count_image -= 1
+        format_ = 'details-first' if count_image.zero?
+      elsif format_ == 'details-first'
+        content << category_article(article) + category_image(article)
+
+        count_image += 1
+        format_ = 'image-first' if count_image == 2
+      end
+    end
+
+    content.html_safe
+
+  end
+
+  def category_image(article)
+    content_tag(:div, class: 'col-6 col-lg-3 p-0') do
+      content_tag(:div, 'style': featured_img(article.image_url).to_s, 
+        class: 'categories-img-height background-img-setting position-relative') do
+          
+      end      
+    end
+  end
+
+  def category_article(article)
+    content_tag(:div, class: 'col-6 col-lg-3 p-0 px-3 py-2 bg-light') do
+      concat content_tag(:span, article.category.name, class: 'border-bottom border-3 border-warning text-warning base-color fw-bolder pb-1')
+
+      concat category_title(article)
+
+      concat content_tag(:p, article.text, class: 'text-truncate')
+
+      concat link_to 'Upvote', "/upvote/#{article.id}", class: "btn base-bg-color text-white"
+    end
+
+  end
+
+  def category_title(article)
+    content_tag(:h5, class: 'mt-2') do
+      link_to article.title, article, class: 'text-dark text-decoration-none'
+    end
+  end
+
 end
