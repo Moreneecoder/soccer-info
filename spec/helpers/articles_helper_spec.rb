debdbcf1fc8f94cc5a08d0252867ebbe8bf4cbb8
Morenikeji Bello <caeserselassie@gmail.com>
2021-06-01 14:27:04 +0100

    Rename helper methods and files

--- spec/helpers/articles_helper_spec.rb
+++ spec/helpers/articles_helper_spec.rb
@@ -10,6 +10,6 @@ require 'rails_helper'
 #     end
 #   end
 # end
-RSpec.describe ArticlesHelper, type: :helper do
+RSpec.describe ArticleHelper, type: :helper do
   pending "add some examples to (or delete) #{__FILE__}"
 end
