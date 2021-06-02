97879a555f0579fdcfcb88b20bc67b6ba470907c
Morenikeji Bello <caeserselassie@gmail.com>
2021-06-02 15:04:56 +0100

    Remove problematic migrations

--- db/migrate/20210517203254_create_articles.rb
+++ db/migrate/20210517203254_create_articles.rb
@@ -5,7 +5,7 @@
 class CreateArticles < ActiveRecord::Migration[6.1]
   def change
     create_table :articles do |t|
-      t.references :author, null: false, foreign_key: true
+      t.references :author, null: false
       t.string :title
       t.text :text
       t.string :image
