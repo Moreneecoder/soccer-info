a5f5389d15992dcff552a931dfc61a14b0fc932c
Morenikeji Bello <caeserselassie@gmail.com>
2021-06-02 16:46:39 +0100

    Fix migration problem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
+    return if session[:user_id].nil?
     @current_user = User.find(session[:user_id]) if session[:user_id]
   end
 end
