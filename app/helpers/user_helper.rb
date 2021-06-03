90e516d1c2d88efd293f2daa67fa57801226cefb
Morenikeji Bello <caeserselassie@gmail.com>
2021-06-03 17:15:49 +0100

    Refactor top_writer logic to use helper method

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,2 +1,14 @@
 module UserHelper
+  def render_top_writer
+
+    if @votes.zero?
+      content_tag(:div, class: 'alert alert-info') do
+        content_tag :h4, '&#9785; No Votes For Top Writer Yet!'
+      end
+    end
+
+    unless @votes.zero?
+      render 'top_writer_details'
+    end
+  end
 end
