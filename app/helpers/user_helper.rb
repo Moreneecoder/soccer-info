234e1e4688f6424f446b79e1c1441ee4f9e0b04d
Morenikeji Bello <caeserselassie@gmail.com>
2021-06-03 17:16:14 +0100

    Fix rubocop errors

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,14 +1,11 @@
 module UserHelper
   def render_top_writer
-
     if @votes.zero?
       content_tag(:div, class: 'alert alert-info') do
         content_tag :h4, '&#9785; No Votes For Top Writer Yet!'
       end
     end
 
-    unless @votes.zero?
-      render 'top_writer_details'
-    end
+    render 'top_writer_details' unless @votes.zero?
   end
 end
